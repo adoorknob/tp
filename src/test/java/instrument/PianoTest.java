2f790d9d508cadc7130a07e1195315a34ecb5982
Jason-Hp <j4sonho520@gmail.com>
2025-03-09 00:53:26 +0800

    Add unit testing for all instruments

---
+++ src/test/java/instrument/PianoTest.java
@@ -0,0 +1,47 @@
+package instrument;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PianoTest {
+
+    @Test
+    void rent_rentPiano_expectTrue() {
+        Piano piano = new Piano("Piano","Yamaha",2000);
+        piano.rent();
+        assertTrue(piano.isRented());
+    }
+
+    @Test
+    void unrent_unrentPiano_expectFalse() {
+        Piano piano = new Piano("Piano","Yamaha",2000);
+        piano.rent();
+        piano.unrent();
+        assertFalse(piano.isRented());
+    }
+
+    @Test
+    void toString_rentedPiano_expectCorrectString() {
+        Piano piano = new Piano("Piano","Yamaha",2000);
+        piano.rent();
+        assertEquals("Piano | X", piano.toString());
+    }
+
+    @Test
+    void toString_unrentedPiano_expectCorrectString() {
+        Piano piano = new Piano("Piano","Yamaha",2000);
+        piano.rent();
+        piano.unrent();
+        assertEquals("Piano | O", piano.toString());
+    }
+
+
+    @Test
+    void playInstrument_beingPlayed_expectPianoSounds(){
+        Piano piano = new Piano("Piano","Yamaha",2000);
+        assertEquals("Piano Sounds", piano.playInstrument());
+    }
+}
