58de73b3d39a6f4c5de70d2484b9321074419089
adoorknob <quantengwai@gmail.com>
2025-03-16 23:50:30 +0800

    Create ParserTest

---
+++ src/test/java/ui/ParserTest.java
@@ -0,0 +1,54 @@
+package ui;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ParserTest {
+    private Parser parser;
+
+    @BeforeEach
+    public void setUp() {
+        parser = new Parser();
+    }
+
+    @Test
+    void parse_validInput_parsedCorrectly() {
+        String validInput = "Guitar | PAC112VM | 1999";
+        String[] intendedOutput = {"Guitar", "PAC112VM", "1999"};
+        try {
+            String[] output = parser.separateNMY(validInput);
+            assertEquals(output, intendedOutput);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to parse input", e);
+        }
+    }
+
+    @Test
+    void parse_invalidInstrument_errorMessage() {
+        String invalidInput = "Guitar | 1999";
+
+        try {
+            parser.separateNMY(invalidInput);
+            fail("Expected IOException to be thrown");
+        } catch (IOException e) {
+            assertEquals("Input instrument is invalid", e.getMessage());
+        }
+    }
+
+    @Test
+    void parse_invalidYear_errorMessage() {
+        String invalidInput = "Guitar | PAC112VM | hehe";
+
+        try {
+            parser.separateNMY(invalidInput);
+            fail("Expected IOException to be thrown");
+        } catch (IOException e) {
+            assertEquals("Input year is invalid", e.getMessage());
+        }
+    }
+}
