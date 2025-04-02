04c73c4415f912178113f5da8350f3fbe9408770
KashfyZul <e1121648@u.nus.edu>
2025-04-02 17:24:19 +0800

    Extended "list" comand into "list" and "list stock"

--- src/main/java/commands/ListCommand.java
+++ src/main/java/commands/ListCommand.java
@@ -1,18 +1,34 @@
 package commands;
 
 import instrument.InstrumentList;
+import parser.CommandParser;
 import ui.Ui;
 
 public class ListCommand extends Command {
+    private CommandParser parser;
     // Constructor
-    public ListCommand() {
-        super("Exit");
+    public ListCommand(String command) {
+        super(command);
+        parser = new CommandParser();
     }
+
 
     @Override
     public void execute(InstrumentList instrumentList, Ui ui) {
-        ui.printInstrumentList(instrumentList.getList());
-        ui.printStockList(instrumentList.getList());
+
+        try {
+            if (this.name.isEmpty()) {
+                ui.printInstrumentList(instrumentList.getList());
+                return;
+            }
+            String[] userInput = parser.splits(this.name);
+            String subCmd = userInput[0];
+            if (subCmd.equals("stock")) {
+                ui.printStockList(instrumentList.getList());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
