383a3f4ab73aa426f3d15d8c1e52121dd7422cff
adoorknob <quantengwai@gmail.com>
2025-04-07 01:52:17 +0800

    Add Storage tests

--- src/test/java/storage/StorageTest.java
+++ src/test/java/storage/StorageTest.java
@@ -1,19 +1,39 @@
 package storage;
 
 import exceptions.storage.FileCannotBeMadeException;
+import instrument.Guitar;
+import instrument.InstrumentList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ui.Ui;
 import user.UserList;
 import user.UserUtils;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StorageTest {
     Ui ui;
     UserList userList;
     UserUtils userUtils;
+
+    static class StubUi extends Ui {
+        boolean directoryCreated = false;
+        boolean fileCreated = false;
+
+        @Override
+        public void printCreatingDirectory(String dir) {
+            directoryCreated = true;
+        }
+
+        @Override
+        public void printCreatingFile(String file) {
+            fileCreated = true;
+        }
+    }
 
     @BeforeEach
     void setUp() {
@@ -23,14 +43,14 @@ public class StorageTest {
     }
 
     @Test
-    public void testLoadOldFileValidFilepath() {
+    public void testLoadOldFile_validFilepath_success() {
         Storage storage = new Storage(ui, userUtils, "src/test/resources/test_file.txt");
 
         assertDoesNotThrow(storage::loadOldFile);
     }
 
     @Test
-    public void testLoadOldFileEmptyFilepath() {
+    public void testLoadOldFile_emptyFilepath_exception() {
         Storage storage = new Storage(ui, userUtils, "");
         try {
             storage.loadOldFile();
@@ -40,8 +60,27 @@ public class StorageTest {
     }
 
     @Test
-    public void testLoadOldFileNoDirectory() {
+    public void testLoadOldFile_noDirectory_success() {
         Storage storage = new Storage(ui, userUtils, "test_file.txt");
         assertDoesNotThrow(storage::loadOldFile);
+    }
+
+    @Test
+    public void testSaveCurrentFile() throws IOException {
+        StubUi stubUi = new StubUi();
+        Storage storage = new Storage(stubUi, userUtils, "test/test_output.txt");
+        storage.loadOldFile();
+
+        Guitar guitar = new Guitar("Guitar", "ModelName", 2000);
+        InstrumentList instrumentList = new InstrumentList();
+        instrumentList.addInstrument(guitar);
+
+        storage.saveCurrentFile(instrumentList);
+
+        assertTrue(stubUi.directoryCreated, "Directory creation was not triggered");
+        assertTrue(stubUi.fileCreated, "File creation was not triggered");
+
+        assertEquals(guitar.toFileEntry() + "\n", storage.outputText,
+                "Output was not written correctly");
     }
 }
