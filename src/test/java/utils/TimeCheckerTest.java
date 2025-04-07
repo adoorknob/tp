dd28b2503cb17d3289ef2c557561e9c722590db0
KashfyZul <e1121648@u.nus.edu>
2025-04-08 02:38:47 +0800

    TimeCheckerTest

---
+++ src/test/java/utils/TimeCheckerTest.java
@@ -0,0 +1,31 @@
+package utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TimeCheckerTest {
+    private TimeChecker timeChecker;
+    @BeforeEach
+    void setUp() {
+        timeChecker = new TimeChecker();
+    }
+
+    @Test
+    void getCurrentYearTest() {
+        Integer year = timeChecker.getCurrentYear();
+        assertEquals(LocalDate.now().getYear(), year);
+    }
+
+    @Test
+    void getCurrentTimeTest() {
+        LocalTime time = timeChecker.getCurrentTime();
+        assertEquals(LocalTime.now(), time);
+    }
+
+
+}
